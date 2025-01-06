544cfc4dc00d7f1472fbaf1af730d175ba8ec182
ddiske <ddiske@naver.com>
2025-01-07 08:58:51 +0900

    ddiske

--- src/com/root/app/langs/ex1/PersonView.java
+++ src/com/root/app/langs/ex1/PersonView.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class PersonView {
 	
-	Scanner
+	
 	
 	public void view(Person [] persons) {
 		System.out.println("이름"+"\t"+"\t"+"전화번호"+"\t"+"\t"+"이메일"+"\t"+"\t"+"생년월일");
