41e29e1ea1afb3c86659832a1200f567d0a23885
ddiske <ddiske@naver.com>
2025-01-07 09:53:26 +0900

    Day10_1

---
+++ src/com/root/app/langs/Study3StringBuffer1.java
@@ -0,0 +1,14 @@
+package com.root.app.langs;
+
+public class Study3StringBuffer1 {
+
+	public static void main(String[] args) {
+		String str = "a";
+		str = str + "b";
+		System.out.println(str);
+		
+		
+
+	}
+
+}
