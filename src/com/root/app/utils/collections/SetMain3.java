7fd668bea4fb5b54fb40d639c913702d570a5dd3
ddiske <ddiske@naver.com>
2025-01-09 14:00:47 +0900

    Day12_2

---
+++ src/com/root/app/utils/collections/SetMain3.java
@@ -0,0 +1,27 @@
+package com.root.app.utils.collections;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class SetMain3 {
+
+	public static void main(String[] args) {
+		
+		HashSet<Integer> hs = new HashSet<>();
+		
+		hs.add(21);
+		hs.add(23);
+		hs.add(22);
+		
+		Iterator<Integer> it = hs.iterator();
+		
+		System.out.println(it);
+		System.out.println(it.getClass());
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+
+	}
+
+}
